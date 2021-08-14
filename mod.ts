e2ea22bccea86db498465b7bfae8706416b91e10
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-08-14 12:20:51 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 addEventListener("fetch", (event) => {
   const searchParams = new URL(event.request.url).searchParams
-  const targetText = searchParams.get('text') ?? 'メロスは激怒した。'
+  const targetText = searchParams.get('text') ?? `吾輩は猫である。名前は${crypto.randomUUID()}`
   const targetLang = searchParams.get('lang') ?? 'ja'
   const segments = new Intl.Segmenter(targetLang, {granularity: "word"}).segment(targetText);
   event.respondWith(
