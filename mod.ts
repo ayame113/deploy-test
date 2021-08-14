3d09903105127113cd477b9463c0024ecb17319d
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-08-14 12:19:24 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,7 +1,8 @@
 addEventListener("fetch", (event) => {
-  const targetText = new URL(event.request.url).searchParams.get('text') ?? 'メロスは激怒した。'
-  const segmenter = new Intl.Segmenter("ja", {granularity: "word"});
-  const segments = segmenter.segment(targetText);
+  const searchParams = new URL(event.request.url).searchParams
+  const targetText = searchParams.get('text') ?? 'メロスは激怒した。'
+  const targetLang = searchParams.get('lang') ?? 'ja'
+  const segments = new Intl.Segmenter(targetLang, {granularity: "word"}).segment(targetText);
   event.respondWith(
     new Response(JSON.stringify([...segments].map(seg=>seg.segment)), {
       status: 200,
