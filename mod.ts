ed2a1661db59b727afe9f6572694810dc3676746
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-08-14 12:04:29 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,6 +1,8 @@
 addEventListener("fetch", (event) => {
+  const segmenter = new Intl.Segmenter("ja", {granularity: "word"});
+  const segments = segmenter.segment('メロスは激怒した。');
   event.respondWith(
-    new Response("Hello world", {
+    new Response(JSON.stringify([...segments]), {
       status: 200,
       headers: {
         "content-type": "text/plain",
